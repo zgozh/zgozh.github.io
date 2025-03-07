7808ba395f3135882f7475d23b06ab3d789e2663
zgozh <13610537014@163.com>
2025-03-07 16:51:09 +0800

    Site updated: 2025-03-07 16:51:08

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/04/cppstart/","2025/03/01/hello-world/"];function toRandomPost(){
+var posts=["2025/03/04/cppstart/","2025/03/06/markdown/","2025/03/01/hello-world/","2025/03/07/waimai/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
